60f4947e5cb06fc8f9f874ced5cc7c554cae6968
Stefan Walter <stefan_walter@posteo.de>
2025-01-01 14:25:00 +0100

    add initial working version

---
+++ README.md
@@ -0,0 +1,19 @@
+A small demo project performing normal-order evaluation of untyped lambda calculus terms.
+It wraps the library https://github.com/stfnw/lambda-calculus-full-reducing-krivine-machine into a small cli app.
+The library implements a full-reducing variant (KN) of the Krivine abstract machine;
+specifically the formulation in Á. GARCÍA-PÉREZ and P. NOGUEIRA, "The full-reducing Krivine abstract machine KN simulates pure normal-order reduction in lockstep: A proof via corresponding calculus", Journal of Functional Programming, vol. 29, p. e7, 2019. doi:10.1017/S0956796819000017; Fig. 3. "A version of KN that works with closures with closed terms".
+(This repo is unaffiliated; goal is me learning).
+
+Example run:
+
+```
+$ cargo run -- '((λa. (a a)) (λa. a))'
+    Finished `dev` profile [unoptimized + debuginfo] target(s) in 0.04s
+     Running `target/debug/lambda-calculus-full-reducing-krivine-machine-cli '((λa. (a a)) (λa. a))'`
+parsed term:
+    named: ((λa. (a a)) (λa. a))
+    blc:   01000110100010
+reduced term:
+    named: (λa. a)
+    blc:   0010
+```
---
+++ src/main.rs
@@ -0,0 +1,49 @@
+use clap::{Parser, ValueEnum};
+use lambda_calculus_full_reducing_krivine_machine::{
+    eval,
+    format::{blc, named},
+};
+
+#[derive(Debug, Parser)]
+#[command(version, about, long_about = None)]
+/// A small demo project performing normal-order evaluation of untyped lambda calculus terms.
+/// It implements a full-reducing variant (KN) of the Krivine abstract machine;
+/// specifically the formulation in Á. GARCÍA-PÉREZ and P. NOGUEIRA, "The full-reducing Krivine abstract machine KN simulates pure normal-order reduction in lockstep: A proof via corresponding calculus", Journal of Functional Programming, vol. 29, p. e7, 2019. doi:10.1017/S0956796819000017; Fig. 3. "A version of KN that works with closures with closed terms".
+/// (This repo is unaffiliated; goal is me learning).
+struct Cli {
+    /// Lambda term that should be evaluated.
+    term: String,
+
+    #[arg(value_enum,  default_value_t=Format::Named)]
+    /// Input format of the provided lambda term.
+    format: Format,
+}
+
+#[derive(Clone, Debug, ValueEnum)]
+enum Format {
+    /// Typical representation using named variables and `λ`/`\` for lambdas. (Requires strict paranthesis).
+    /// Example: ((λa. (a a)) (λa. a))
+    Named,
+
+    /// John Tromp's Binary Lambda Calculus encoding format
+    /// (see https://tromp.github.io/cl/Binary_lambda_calculus.html#lambda_encoding).
+    /// Example: 01000110100010
+    Blc,
+}
+
+fn main() {
+    let cli = Cli::parse();
+
+    let term = match cli.format {
+        Format::Named => named::decode(&cli.term),
+        Format::Blc => blc::decode(&cli.term),
+    };
+    println!("parsed term:");
+    println!("    named: {}", term);
+    println!("    blc:   {}", blc::encode(&term));
+
+    let reduced = eval(term);
+    println!("reduced term:");
+    println!("    named: {}", reduced);
+    println!("    blc:   {}", blc::encode(&reduced));
+}
